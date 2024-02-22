dd7232cf3a6a818108eb5408687f2fba98159a46
Sunny <sunny2022.za@gmail.com>
2024-02-22 22:40:21 +0800

    debug1

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -500,7 +500,7 @@ func tryGetOptimizedCode(evm *EVM, addrCopy common.Address) (bool, []byte) {
 	}
 
 	if evm.Context.BlockNumber.Uint64() == uint64(455082) {
-		log.Warn("======= gasps ======", "block", evm.Context.BlockNumber.Uint64(), "hit", optimized)
+		log.Warn("======= gasps ======", "block", evm.Context.BlockNumber.Uint64(), "address", addrCopy, "hit", optimized)
 		log.Warn("======= gasps ======", "block", evm.Context.BlockNumber.Uint64(), "original code::", common.Bytes2Hex(evm.StateDB.GetCode(addrCopy)))
 		if optimized {
 			log.Warn("======= gasps ======", "block", evm.Context.BlockNumber.Uint64(), "optimized code::", common.Bytes2Hex(evm.StateDB.GetCode(addrCopy)))
